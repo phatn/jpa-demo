930212a828e876e33e04687ce786e72976aedc49
Phat Nguyen <phatnguyentanit@gmail.com>
2019-05-25 11:21:34 +0700

    Made Product as an entity

--- src/main/java/com/musictribe/demo/domain/Product.java
+++ src/main/java/com/musictribe/demo/domain/Product.java
@@ -1,9 +1,19 @@
 package com.musictribe.demo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+	@Column(name = "name")
 	private String name;
 
 	public int getId() {
